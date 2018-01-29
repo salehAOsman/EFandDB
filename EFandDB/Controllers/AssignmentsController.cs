19457be01b2932a44152a83b3d47c13d0e9a67a9
Saleh Osman <saleh1.a.osman@gmail.com>
2018-01-29 09:59:12 +0100

    New classes

--- EFandDB/Controllers/AssignmentsController.cs
+++ EFandDB/Controllers/AssignmentsController.cs
@@ -46,7 +46,7 @@ namespace EFandDB.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Name")] Assignment assignment)
+        public ActionResult Create([Bind(Include = "Id,Name,Descriptions")] Assignment assignment)
         {
             if (ModelState.IsValid)
             {
@@ -78,7 +78,7 @@ namespace EFandDB.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Name")] Assignment assignment)
+        public ActionResult Edit([Bind(Include = "Id,Name,Descriptions")] Assignment assignment)
         {
             if (ModelState.IsValid)
             {
